859651bc8decc5c73fbd6d6e9fcc16ca03ee1008
Devanand-PA <devanandpa721@gmail.com>
2023-09-22 08:54:06 +0530

    added bash_profile properly

--- installer.sh
+++ installer.sh
@@ -17,5 +17,11 @@ cd ..
 cp $HOME/.bashrc $HOME/.bashrc."$(date +%s%N)".bak
 cp bashrc $HOME/.bashrc
 
+cp $HOME/.xinitrc $HOME/.xinitrc."$(date +%s%N)".bak
+cp xinitrc $HOME/.xinitrc
+
+cp $HOME/.bash_profile $HOME/.bash_profile."$(date +%s%N)".bak
+cp bash_profile $HOME/.bash_profile
+
 mkdip -p $HOME/.local/bin
 cp scripts/ $HOME/.local/bin/
