7ce2af63d156c7ab3a10a0cb08816548e4ecefef
Devanand-PA <devanandpa721@gmail.com>
2023-11-02 11:31:02 +0530

    Added colors to dwm

---
+++ st/colors-light.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+	/* 8 normal colors */
+	"#666666",
+	"#990000",
+	"#009900",
+	"#999900",
+	"#009999",
+	"#990099",
+	"#777799",
+	"#111111",
+
+	/* 8 bright colors */
+	"#557755",
+	"#550000",
+	"#005500",
+	"#555500",
+	"#005555",
+	"#550055",
+	"#111155",
+	"#111111",
+
+	[255] = 0,
+
+	/* more colors can be added after 555 to use with DefaultXX */
+	"#cccccc",
+	"#555555",
+	"#000000", /* default foreground colour */
+	"#666666", /* default background colour */
+};
