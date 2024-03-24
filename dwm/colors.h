c6073b9a52ab18164fe91639a2857b1a0a16b272
Devanand-PA <devanandpa721@gmail.com>
2024-03-24 11:07:55 +0530

    Changed dwm colors

--- dwm/colors.h
+++ dwm/colors.h
@@ -5,8 +5,12 @@ static const char col_2[]       = "#444444";
 static const char col_3[]       = "#449911";
 static const char col_4[]       = "#000000";
 static const char col_5[]       = "#449911";
+static const char col_6[]       = "#449911";
+static const char col_7[]       = "#040404";
+static const char col_8[]       = "#333333";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_3, col_1, col_2 },
 	[SchemeSel]  = { col_4, col_5,  col_5  },
+	[SchemeTitle]  = { col_6, col_7,  col_8  },
 };
