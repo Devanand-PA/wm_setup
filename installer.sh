ed70a4dc39567bad8734d6888b0d08efcaf17350
Devanand-PA <devanandpa721@gmail.com>
2024-05-03 02:28:51 +0530

    Added ttf-firacode as dependency

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-Debian_deps="gcc make libxinerama-dev libxft-dev fontconfig fonts-liberation libx11-dev xinit x11-session-utils build-essential fonts-noto-cjk-extra fonts-font-awesome libxcb-xinerama0-dev libxcb1-dev libx11-xcb-dev libx11-xcb-dev bsdmainutils libxcb-res0-dev build-essential make alacritty python3-venv "
-Arch_deps="libxinerama libxft fontconfig ttf-liberation xorg-xinit curl wget base-devel alacritty"
+Debian_deps="gcc make libxinerama-dev libxft-dev fontconfig fonts-liberation libx11-dev xinit x11-session-utils build-essential fonts-noto-cjk-extra fonts-font-awesome libxcb-xinerama0-dev libxcb1-dev libx11-xcb-dev libx11-xcb-dev bsdmainutils libxcb-res0-dev build-essential make alacritty python3-venv fonts-firacode"
+Arch_deps="libxinerama libxft fontconfig ttf-liberation xorg-xinit curl wget base-devel alacritty ttf-fira-code"
 Common_utils="fonts-jetbrains-mono alsa-utils acpi neovim links sxiv mpv fzf curl wget htop brightnessctl tk"
 sudo apt install nala
 sudo pacman -Sy $Arch_deps $Common_utils ||  sudo nala  install $Debian_deps $Common_utils
