7df829ceeb6f41666e43f460c0b4390c41731a0b
Devanand-PA <devanandpa721@gmail.com>
2024-05-14 21:17:27 +0530

    removed a useless file

--- dmenu/colors.h
+++ dmenu/colors.h
@@ -1,7 +1,7 @@
 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#c8c8c9", "#00001A" },
-	[SchemeSel] = {  "#00001A", "#115663" },
-	[SchemeOut] = {  "#c8c8c9", "#728B95" },
+	[SchemeNorm] = { "#c8b499", "#000000" },
+	[SchemeSel] = {  "#090B13", "#453941" },
+	[SchemeOut] = {  "#c8b499", "#946B44" },
 };
