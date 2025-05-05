0f7c1b6f75ea394f37ca6b718f61ee6777d31266
Devanand-PA <devanandpa721@gmail.com>
2025-05-05 16:57:32 +0530

    updated subplay

--- dwm/colors.h
+++ dwm/colors.h
@@ -1,9 +1,9 @@
 
 static char normbordercolor[] =  "#000000" ;
-static char normfgcolor[] = "#b4cbda";
-static char normbgcolor[] = "#0F1231";
-static char selbordercolor[] = "#b4cbda";
-static char selbgcolor[] = "#58728E";
-static char selfgcolor[] = "#0F1231";
+static char normfgcolor[] = "#e0e0e0";
+static char normbgcolor[] = "#020101";
+static char selbordercolor[] = "#e0e0e0";
+static char selbgcolor[] = "#C7021E";
+static char selfgcolor[] = "#020101";
 static char dwm_titlebg[] = "#000000";
-static char dwm_titlefg[] = "#b4cbda";
+static char dwm_titlefg[] = "#e0e0e0";
--- st/colors.h
+++ st/colors.h
@@ -1,29 +1,29 @@
 
 static const char *colorname[] = {
-"#0F1231",      //  "black",
-"#1C4384",      //  "red3",
-"#58728E",      //  "green3",
-"#328CA7",      //  "yellow3",
-"#7096AF",      //  "blue2",
-"#7395B0",      //  "magenta3",
-"#61A2C2",      //  "cyan3",
-"#b4cbda",      //  "#444444",
-"#7d8e98",      //  "gray50",
-"#1C4384",      //  "red",                        
-"#58728E",     //  "green",
-"#328CA7",     //  "yellow",
-"#7096AF",     //  "#5c5cff" "light blue"
-"#7395B0",     //  "magenta"
-"#61A2C2",     //  "cyan",
-"#b4cbda",     //  "white",
+"#020101",      //  "black",
+"#AB0624",      //  "red3",
+"#C7021E",      //  "green3",
+"#DE0025",      //  "yellow3",
+"#E30127",      //  "blue2",
+"#817B7D",      //  "magenta3",
+"#A3A2A2",      //  "cyan3",
+"#e0e0e0",      //  "#444444",
+"#9c9c9c",      //  "gray50",
+"#AB0624",      //  "red",                        
+"#C7021E",     //  "green",
+"#DE0025",     //  "yellow",
+"#E30127",     //  "#5c5cff" "light blue"
+"#817B7D",     //  "magenta"
+"#A3A2A2",     //  "cyan",
+"#e0e0e0",     //  "white",
                             
 [255] = 0,
 
 /* more colors can be added after 255 to use with DefaultXX */
 "#444444",
 "#555555",
-"#b4cbda",
-"#0F1231", /* default background color */
+"#e0e0e0",
+"#020101", /* default background color */
 };
 /* bg opacity */
 float alpha = 0.75;
