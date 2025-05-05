2034f253519874295c2c970ecfd5840cfe7d60ff
Devanand-PA <devanandpa721@gmail.com>
2025-05-05 16:57:32 +0530

    updated subplay

--- dmenu/colors.h
+++ dmenu/colors.h
@@ -1,7 +1,7 @@
 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#b4cbda", "#0F1231" },
-	[SchemeSel] = {  "#0F1231", "#1C4384" },
-	[SchemeOut] = {  "#b4cbda", "#61A2C2" },
+	[SchemeNorm] = { "#e0e0e0", "#020101" },
+	[SchemeSel] = {  "#020101", "#AB0624" },
+	[SchemeOut] = {  "#e0e0e0", "#A3A2A2" },
 };
