fc9250d7609502482a0581694b6879216e21b927
Devanand-PA <devanandpa721@gmail.com>
2026-09-14 12:12:05 +0530

    I might have messed up sel_img, but atleast it has a gallery mode

--- dmenu/colors.h
+++ dmenu/colors.h
@@ -1,7 +1,7 @@
 
 static const char *colors[SchemeLast][2] = {
     /*     fg         bg       */
-    [SchemeNorm] = { "#f0e5cb", "#0D161D" },
-    [SchemeSel]  = { "#0D161D", "#6D675A" },
-    [SchemeOut]  = { "#C1AA92", "#f0e5cb" },
+    [SchemeNorm] = { "#ccd5e2", "#252220" },
+    [SchemeSel]  = { "#252220", "#5C78A9" },
+    [SchemeOut]  = { "#E1D3A3", "#ccd5e2" },
 };
--- dwm/colors.h
+++ dwm/colors.h
@@ -1,9 +1,9 @@
 
 static char normbordercolor[] = "#000000";
-static char normfgcolor[] = "#f0e5cb";
-static char normbgcolor[] = "#0D161D";
-static char selbordercolor[] = "#f0e5cb";
-static char selbgcolor[] = "#8E765F";
-static char selfgcolor[] = "#0D161D";
+static char normfgcolor[] = "#ccd5e2";
+static char normbgcolor[] = "#252220";
+static char selbordercolor[] = "#ccd5e2";
+static char selbgcolor[] = "#6682B2";
+static char selfgcolor[] = "#252220";
 static char dwm_titlebg[] = "#000000";
-static char dwm_titlefg[] = "#f0e5cb";
+static char dwm_titlefg[] = "#ccd5e2";
