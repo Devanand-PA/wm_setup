a241f4dc39550321817014f6dc38477af8fb0e0a
Devanand-PA <devanandpa721@gmail.com>
2026-09-14 14:29:22 +0530

    sped up sel_img

--- dmenu/colors.h
+++ dmenu/colors.h
@@ -1,7 +1,7 @@
 
 static const char *colors[SchemeLast][2] = {
     /*     fg         bg       */
-    [SchemeNorm] = { "#ccd5e2", "#252220" },
-    [SchemeSel]  = { "#252220", "#5C78A9" },
-    [SchemeOut]  = { "#E1D3A3", "#ccd5e2" },
+    [SchemeNorm] = { "#decfbe", "#0f0e0c" },
+    [SchemeSel]  = { "#0f0e0c", "#8B7168" },
+    [SchemeOut]  = { "#A19CA0", "#decfbe" },
 };
--- dwm/colors.h
+++ dwm/colors.h
@@ -1,9 +1,9 @@
 
 static char normbordercolor[] = "#000000";
-static char normfgcolor[] = "#ccd5e2";
-static char normbgcolor[] = "#252220";
-static char selbordercolor[] = "#ccd5e2";
-static char selbgcolor[] = "#6682B2";
-static char selfgcolor[] = "#252220";
+static char normfgcolor[] = "#decfbe";
+static char normbgcolor[] = "#0f0e0c";
+static char selbordercolor[] = "#decfbe";
+static char selbgcolor[] = "#BFA36E";
+static char selfgcolor[] = "#0f0e0c";
 static char dwm_titlebg[] = "#000000";
-static char dwm_titlefg[] = "#ccd5e2";
+static char dwm_titlefg[] = "#decfbe";
